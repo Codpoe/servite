4c6d8aadb0ad69e671f3401057249146db9ab509
codpoe <codpoe.me@gmail.com>
2022-07-25 10:29:00 +0800

    feat: add cli

---
+++ src/node/cli.tsx
@@ -0,0 +1,114 @@
+import { fileURLToPath } from 'url';
+import fs from 'fs-extra';
+import { cac } from 'cac';
+import colors from 'picocolors';
+import type { BuildOptions, LogLevel } from 'vite';
+
+const pkgPath = fileURLToPath(new URL('../../package.json', import.meta.url));
+const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
+
+// global options
+interface GlobalCLIOptions {
+  '--'?: string[];
+  c?: boolean | string;
+  config?: string;
+  base?: string;
+  l?: LogLevel;
+  logLevel?: LogLevel;
+  clearScreen?: boolean;
+  d?: boolean | string;
+  debug?: boolean | string;
+  f?: string;
+  filter?: string;
+  m?: string;
+  mode?: string;
+  force?: boolean;
+}
+
+/**
+ * removing global flags before passing as command specific sub-configs
+ */
+function cleanOptions<Options extends GlobalCLIOptions>(
+  options: Options
+): Omit<Options, keyof GlobalCLIOptions> {
+  const ret = { ...options };
+  delete ret['--'];
+  delete ret.c;
+  delete ret.config;
+  delete ret.base;
+  delete ret.l;
+  delete ret.logLevel;
+  delete ret.clearScreen;
+  delete ret.d;
+  delete ret.debug;
+  delete ret.f;
+  delete ret.filter;
+  delete ret.m;
+  delete ret.mode;
+  return ret;
+}
+
+const cli = cac('servite').version(pkg.version).help();
+
+// build
+// based on vite build
+cli
+  .command('build [root]', 'build for production')
+  .option('--target <target>', `[string] transpile target (default: 'modules')`)
+  .option('--outDir <dir>', `[string] output directory (default: dist)`)
+  .option(
+    '--assetsDir <dir>',
+    `[string] directory under outDir to place assets in (default: assets)`
+  )
+  .option(
+    '--assetsInlineLimit <number>',
+    `[number] static asset base64 inline threshold in bytes (default: 4096)`
+  )
+  .option(
+    '--ssr [entry]',
+    `[string] build specified entry for server-side rendering`
+  )
+  .option(
+    '--sourcemap',
+    `[boolean] output source maps for build (default: false)`
+  )
+  .option(
+    '--minify [minifier]',
+    `[boolean | "terser" | "esbuild"] enable/disable minification, ` +
+      `or specify minifier to use (default: esbuild)`
+  )
+  .option('--manifest [name]', `[boolean | string] emit build manifest json`)
+  .option('--ssrManifest [name]', `[boolean | string] emit ssr manifest json`)
+  .option(
+    '--force',
+    `[boolean] force the optimizer to ignore the cache and re-bundle (experimental)`
+  )
+  .option(
+    '--emptyOutDir',
+    `[boolean] force empty outDir when it's outside of root`
+  )
+  .option('-w, --watch', `[boolean] rebuilds when modules have changed on disk`)
+  .action(async (root: string, options: BuildOptions & GlobalCLIOptions) => {
+    // TODO: use custom build
+    const { build, createLogger } = await import('vite');
+    const buildOptions: BuildOptions = cleanOptions(options);
+
+    try {
+      await build({
+        root,
+        base: options.base,
+        mode: options.mode,
+        configFile: options.config,
+        logLevel: options.logLevel,
+        clearScreen: options.clearScreen,
+        optimizeDeps: { force: options.force },
+        build: buildOptions,
+      });
+    } catch (e: any) {
+      createLogger(options.logLevel).error(
+        colors.red(`error during build:\n${e.stack}`),
+        { error: e }
+      );
+      process.exit(1);
+    }
+  });
