3ce956e2c349ca3f74c8c08607c7f6e4ec6fc46b
codpoe <codpoe.me@gmail.com>
2024-09-30 15:19:33 +0800

    chore: gh-pages & vercel

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'servite/config';
 
 export default defineConfig({
   server: {
+    baseURL: process.env.GH_PAGES ? '/servite' : '/',
     prerender: {
       routes: ['/*', '/**/*'],
       failOnError: true,
